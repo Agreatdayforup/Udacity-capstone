65148b7f0f644f9a7dbb9943b8e469dec77ca4a2
Patrick <cayerp@gmail.com>
2020-02-22 12:17:04 -0500

    combine fetch
    chained fetch requests to work for geonames and darksky api

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -7,9 +7,7 @@ let geoCountry = {};
 let geoCC = {};
 let geoLatit = {};
 let geoLongi = {};
-// let darkTemp = {};
-// let darkSummary = {};
-// let darkIcon = {};
+let pixaData = document.querySelector('.pixabayImage');
 
 
 let degree = document.querySelector('.degree');
@@ -18,29 +16,44 @@ let date = document.querySelector('#date');
 let city = document.querySelector('.dest');
 let resCityTitle = document.querySelector('.resCity')
     
-// Helps prevent Cors error
-const proxy = 'https://cors-anywhere.herokuapp.com/';
+
+
+// darkAPI function
+
+//const getDarkSky = (geoLatit, geoLongi) => {
+
+    // Helps prevent Cors error
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+
+    // DarkSky API information
+    const darkApi = `${proxy}https://api.darksky.net/forecast/1469add55d2500c04eddbd73805930c6/${geoLatit},${geoLongi}`;
+
+
 
 // Destination date for user
     const destDate = date.value
 // Destination city for user
     const destCity = city.value;
 
+//form submittion with API Requests inside
 
+//function handleSubmit(event) {
 searchForm.addEventListener('submit', (e) => {
     e.preventDefault()
     console.log('testing')
    
 
-    resCityTitle.textContent = `The Weather for your trip to ${destCity} on ${destDate} will be!`
+    resCityTitle.textContent = `The current weather in ${destCity} is!`
             console.log(destCity)
             console.log(destDate)
 
+
+
     //Geonames APIusername and api address
     const geoUser = 'agreatdayfor';
-    const geoNamesApi = `http://api.geonames.org/searchJSON?q=${destCity}&maxRows=10&username=${geoUser}`;
+    const geoNamesApi = `http://api.geonames.org/searchJSON?q=${destCity}&maxRows=1&username=${geoUser}`;
      
-    // Geonames API Fetch
+// Geonames API Fetch
      fetch(geoNamesApi)
      .then(response => {
          return response.json();
@@ -55,36 +68,49 @@ searchForm.addEventListener('submit', (e) => {
         console.log(geoLatit)
         console.log(geoLongi)
         console.log(data);
-    })
+
+     return fetch(`${proxy}https://api.darksky.net/forecast/1469add55d2500c04eddbd73805930c6/${geoLatit},${geoLongi}`).then(response => {
+            return response.json();
+        
+        }).then(data => {
+            console.log(data);
+            const { temperature, summary, icon } = data.currently;
+            
+            //set Dom from API
+            degree.textContent = temperature;
+            tempDescription.textContent = summary;
+            // Set Icon
+            setIcons(icon, document.querySelector('.icon'));
 
 
-    // DarkSky API information
-    const darkApi = `${proxy}https://api.darksky.net/forecast/1469add55d2500c04eddbd73805930c6/${geoLatit},${geoLongi}`;
+            // Pixabay API Information
+            const pixaKey = '14937162-57809441d2782a1b475398b82';
+            const pixabayApi = `https://pixabay.com/api/?key=${pixaKey}&q=${destCity}&orientation=horizontal`
 
-    fetch(darkApi)
+            fetch(pixabayApi)
             .then(response => {
                 return response.json();
             })
             .then(data => {
-                console.log(data);
-                const { temperature, summary, icon } = data.currently;
-
-                //set Dom from API
-                degree.textContent = temperature;
-                tempDescription.textContent = summary;
-                // Set Icon
-                setIcons(icon, document.querySelector('.icon'));
+                document.getElementById('.response').style.backgroundImage = data.largeImageURL;
             })
-
-             // function to change icon to a gif version  from DevED youtube
+            
+            // function to change icon to a gif version  from DevED youtube
             function setIcons(icon, iconID) {
-                const skycons = new Skycons({color: 'white'});
-                const currentIcon = icon.replace(/-/g, '_').toUpperCase();
+            const skycons = new Skycons({color: 'white'});
+            const currentIcon = icon.replace(/-/g, '_').toUpperCase();
                 skycons.play();
                 return skycons.set(iconID, Skycons[currentIcon]);
             }
+    })
+        
+    })
 
-})
+    
+    
+ 
+
+
             
 
 
@@ -97,4 +123,17 @@ searchForm.addEventListener('submit', (e) => {
 // let d = new Date();
 // let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
+   //             // Pixabay API Information
+    // const pixaKey = '14937162-57809441d2782a1b475398b82';
+    // const pixabayApi = `https://pixabay.com/api/?key=${pixaKey}&q=${destCity}&orientation=horizontal`
 
+    // fetch(pixabayApi)
+    //  .then(response => {
+    //      return response.json();
+    //  })
+    //  .then(data => {
+    //     document.getElementById(".response").style.backgroundImage = data.largeImageURL;
+            
+
+
+})
