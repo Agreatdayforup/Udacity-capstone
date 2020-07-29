b4f5efe0ff393a7ccc3f5432966dfc7711cbdb51
Patrick C <35810793+Agreatdayforup@users.noreply.github.com>
2020-07-29 16:47:49 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 console.log('the client side is loaded');
 
 // import program
-import {timer} from "./client/js/timer";
+import {timer} from "./js/timer";
 
 //eventListener import
-import './client/js/app';
+import './js/app';
 
 // import Styles
-import './client/styles/main.scss';
+import './styles/main.scss';
 
 export {timer};
